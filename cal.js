7648ec2a11f2d5af4564eaa9bda9ef98fe9f4143
bhavyachoudary <bhavya.mullapudi@neosoftmail.com>
2021-09-21 19:07:41 +0530

    cal2

---
+++ cal.js
@@ -0,0 +1,43 @@
+var resultE1=document.getElementById("result")
+var resultE1=document.getElementById("result")
+var ulEl=document.getElementById("history");
+
+
+
+function answer(eachValue){
+
+    document.getElementById("result").value += eachValue;
+   
+}
+  
+
+function result(){
+
+    let firstVal = document.getElementById("result").value;
+
+    let secondVal = eval(firstVal);
+    let list = firstVal + " = "+ secondVal 
+    console.log(list)
+
+
+    document.getElementById("result").value = secondVal;
+    addlist(list)
+}
+
+function clearall(){
+
+    document.getElementById("result").value=""
+}
+
+
+
+
+function addlist(each) {
+    let listEl = document.createElement("li");
+    listEl.classList.add("eachHistory")
+    listEl.textContent = each
+
+    ulEl.appendChild(listEl)
+}
+
+
